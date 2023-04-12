b92def17e124d7722bda6c1530b2b67f575ef322
megath <megath@live.com>
2023-04-12 16:03:28 -0600

    cambios paginas

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+
+/* 
+window.addEventListener('load', function() {
+    let nav=this.document.getElementById("nav-bar")
+    let footer=this.document.getElementById("foot-er")
+
+    nav.innerHTML=`
+    
+    `;
+    footer.innerHTML=`
+    
+    `;
+
+});
+ */
+
