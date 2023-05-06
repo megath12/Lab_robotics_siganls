e9e896d5dafc408a1bc520638453a54422e1382a
megath <megath@live.com>
2023-05-05 23:56:16 -0600

    5mayo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,10 +21,10 @@ nav.innerHTML=`
       <div class="div-nav navbar-nav ms-auto">
         <a class="nav-link" href="./index.html">Inicio</a>
         <a class="nav-link" href="./publicaciones.html">Publicaciones</a>
-        <a class="nav-link" href="./integrantes.html">Integrantes</a>
-        <a class="nav-link" href="./reconocimientos.html">Reconocimientos</a>
+        <a class="nav-link" href="./integrantes.html">Nosotros</a>
   <!--             <a class="nav-link" href="./investigacion.html">Líneas de Investigación</a> -->
         <a class="nav-link" href="./contacto.html">Contáctanos</a>
+        <a class="logo-aw" href="./reconocimientos.html"> <i class="fa-solid fa-trophy"></i> </a>
         <a class="logo-ig" href="https://www.instagram.com/med_robotics_biosignals_lab/?igshid=YmMyMTA2M2Y%3D" target="_blank"><i class="fa-brands fa-instagram fa-xl"></i></a>
         <a class="logo-yt " href="https://www.instagram.com/med_robotics_biosignals_lab/?igshid=YmMyMTA2M2Y%3D" target="_blank"><i class="fa-brands fa-youtube fa-xl"></i></a>
       </div>
