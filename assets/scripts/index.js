ca0288ed266c2ab3638b2119c007b892a6673bde
megath <megath@live.com>
2023-05-26 23:54:48 -0600

    index 26-5

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,16 @@
 
+var texto = "Medical Robotics and Biosignals Laboratory implementa técnicas de matemática aplicada y novedosas tecnologías para el desarrollo de soluciones biomédicas integrales.";
+var index = 0;
 
+function mostrarTexto() {
+  var miTexto = document.getElementById("descripcion");
+  miTexto.textContent += texto[index];
+  index++;
 
+  if (index >= texto.length) {
+    clearInterval(intervalo);
+  }
+}
+
+var intervalo = setInterval(mostrarTexto, 100); // Ajusta el valor para controlar la velocidad de aparición
 
